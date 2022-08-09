d11c92c8ac61ee715ca55572e914a23600ed334a
mklaicogido123 <mklaicogido123@gmail.com>
2022-08-09 19:05:30 +0700

    first commit

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Button, Input } from "@chakra-ui/react";
+import socketIOClient from "socket.io-client";
 
 import "./message.css";
+
+const host = "http://localhost:8000";
 
 const Message = () => {
   const user = useSelector((state) => state.auth.login.currentUser);
   const [listChat, setListChat] = useState([]);
   const [listMessage, setListMessage] = useState([]);
+  const [chatId, setChatId] = useState("");
+  const [message, setMessage] = useState("");
+  const socketRef = useRef();
 
   const getUserChats = async () => {
     try {
@@ -20,11 +26,25 @@ const Message = () => {
       console.log(error.message);
     }
   };
-  const getMessages = async (chatId) => {
+  const getMessages = async (Id) => {
     try {
-      const res = await axios.get(`/message/${chatId}`);
+      const res = await axios.get(`/message/${Id}`);
       setListMessage(res.data);
-      console.log(res.data);
+      setChatId(Id);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const handleSendMessage = async () => {
+    if (message.trim() === "") return;
+    try {
+      await axios.post(`/message`, {
+        chatId: chatId,
+        senderId: user._id,
+        text: message,
+      });
+      await socketRef.current.emit("sendDataClient", chatId);
+      setMessage("");
     } catch (error) {
       console.log(error.message);
     }
@@ -32,15 +52,45 @@ const Message = () => {
 
   useEffect(() => {
     getUserChats();
+    socketRef.current = socketIOClient.connect(host);
+    socketRef.current.emit("sendDataClient", "Avs");
+
+    socketRef.current.on("sendDataServer", (dataGot) => {
+      const json = JSON.parse(dataGot);
+      // console.log("datagot" + dataGot);
+      // console.log(json);
+      setListMessage(json);
+    });
+    // socketRef.current.emit("baby", "what is going on");
+    // socketRef.current.on("sendDataServer", (data) => {
+    //   setListMessage(data);
+    // });
+
+    return () => {
+      socketRef.current.disconnect();
+    };
     //eslint-disable-next-line
   }, []);
+
+  // const sendMessageSocket = () => {
+  //   socketRef.current.emit("sendDataClient", {
+  //     chatId: chatId,
+  //     senderId: user._id,
+  //     text: message,
+  //   });
+  // };
   return (
     <div className="message">
       <div className="listChat">
         {listChat?.map((chat) => {
           if (chat.members[0]._id === user._id) {
             return (
-              <div className="chat" onClick={() => getMessages(chat._id)}>
+              <div
+                className="chat"
+                onClick={() => {
+                  getMessages(chat._id);
+                }}
+              >
                 <img
                   src={chat.members[1].image}
                   alt="avatar"
@@ -53,7 +103,12 @@ const Message = () => {
             );
           } else {
             return (
-              <div className="chat" onClick={() => getMessages(chat._id)}>
+              <div
+                className="chat"
+                onClick={() => {
+                  getMessages(chat._id);
+                }}
+              >
                 <img
                   src={chat.members[0].image}
                   alt="avatar"
@@ -67,9 +122,9 @@ const Message = () => {
       </div>
       <div className="box-chat">
         <div className="box-chat_message">
-          {listMessage?.map((message) => {
+          {listMessage?.map((message, key) => {
             return (
-              <div>
+              <div key={key}>
                 <p
                   className={
                     message.senderId === user._id
@@ -89,11 +144,17 @@ const Message = () => {
             <p className="other-people">Hii</p>
           </div> */}
         </div>
-
         <div className="send-box">
           {/* <textarea placeholder="Nhập tin nhắn ..." /> */}
-          <Input placeholder="Nhập tin nhắn ..." className="textarea" />
-          <Button colorScheme="green">Send</Button>
+          <Input
+            placeholder="Nhập tin nhắn ..."
+            className="textarea"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+          <Button colorScheme="green" onClick={() => handleSendMessage()}>
+            Send
+          </Button>
         </div>
       </div>
     </div>
